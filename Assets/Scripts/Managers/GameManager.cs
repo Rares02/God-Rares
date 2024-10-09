05ee05eb7fa7bed9a13d05a0809a06a5d2e60759
Rares <rares.marchis02@gmail.com>
2024-10-09 15:33:06 +0200

    Fixato link al google sheet.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     GoogleSheet.GoogleSheet buildingsData;
-    const string linkToSheet = "https://sheets.googleapis.com/v4/spreadsheets/14jqLb3pwnneEam54HM05yes5jRJkelrUBzp3YRgRKA4/values/Foglio1?key=AIzaSyCAqTvJSJqZE_RLfk03HQKQj2EEGjbK1Cg";
+    const string linkToSheet = "https://sheets.googleapis.com/v4/spreadsheets/14jqLb3pwnneEam54HM05yes5jRJkelrUBzp3YRgRKA4/values/Edifici?key=AIzaSyCAqTvJSJqZE_RLfk03HQKQj2EEGjbK1Cg";
    
     private IEnumerator Start() {       
         yield return GoogleSheetToUnity.ObtainSheetData(linkToSheet, result => { buildingsData = result; });
--- Assets/Scripts/Resource/Resource.cs
+++ Assets/Scripts/Resource/Resource.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource : MonoBehaviour {
+public class Resource {
 
     private float quantity;
 
